645cc41d47ecdf7bab39383f8e677739bec6f940
roberto-palanga <r.palanga94@gmail.com>
2024-04-29 19:44:17 +0200

    Inserimento chiamata api e apiCall

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,46 @@ import axios from 'axios';
 export default {
   data() {
     return {
+
+      projects: [],
+
+      apiLink: [],
       
+      apiLink: [],
+      apiPage: 1,
+
+      baseApiUrl: 'http://127.0.0.1:8000/api',
+
+      loader: true,
       
     }
 
   },
 
   mounted() {
-   
+   this.apiCall()
     
   },
 
   methods: {
-    
+    apiCall(){
+      this.loader = true;
+      axios.get( this.baseApiUrl + '/project', {
+        params:{
+          page: this.apiPage
+        }
+      }).then(res => {
+        if(res.data.succes){
+          this.loader = false;
+
+        }
+        this.projects = res.data.results.data;
+        this.apiLink = res.data.results.links;
+      })
+    }
   },
+  
+
 }
 </script>
 
