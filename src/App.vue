278bd6aac2b38ee70d2f600793ef684c47182466
roberto-palanga <r.palanga94@gmail.com>
2024-04-30 15:43:01 +0200

    Commit eseguiti da GianMarco Pimentel

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,8 @@ export default {
       projects: [],
 
       apiLink: [],
-      
-      apiLink: [],
-      apiPage: 1,
+
+      apiPage: [],
 
       baseApiUrl: 'http://127.0.0.1:8000/api',
 
@@ -34,16 +33,24 @@ export default {
           page: this.apiPage
         }
       }).then(res => {
-        if(res.data.success){
-          this.loader = false;
+        // if(res.data.success){
+        //   this.loader = false;
 
-        }
+        // }
+        console.log(res)
         this.projects = res.data.results.data;
         this.apiLink = res.data.results.links;
       })
     }
   },
-  
+  changePage(numberPage){
+    //console.log(numberPage)
+
+    this.apiPage = numberPage;
+
+    //una volta che ho lo stesso numero della pagina rifaccio la chimta api
+    this.apiCall();
+    },
 
 
 
@@ -55,6 +62,14 @@ export default {
 <template>
 <div class="container py-5 display-2 ">
   Progetto di gruppo
+  <ul>
+    <li v-for="project in projects">
+      {{ project.name }}
+      
+    </li>
+  </ul>
+
+  
 </div>
 </template>
 
