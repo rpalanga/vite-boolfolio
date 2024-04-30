4f0e1c2082c10fed08730d433749077815fd56af
roberto-palanga <r.palanga94@gmail.com>
2024-04-30 16:48:36 +0200

    inserimento stile

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script >
+<script>
 //importo axios
 import axios from 'axios';
 
@@ -8,71 +8,111 @@ export default {
 
       projects: [],
 
-      apiLink: [],
+      apiLinks: [],
 
-      apiPage: [],
+      apiPage: 1,
 
       baseApiUrl: 'http://127.0.0.1:8000/api',
 
       loader: true,
-      
+
     }
 
   },
 
   mounted() {
-   this.apiCall()
-    
+    this.apiCall()
+
   },
 
   methods: {
-    apiCall(){
+    apiCall() {
       this.loader = true;
       axios.get(this.baseApiUrl + '/projects', {
-        params:{
+        params: {
           page: this.apiPage
         }
       }).then(res => {
-        // if(res.data.success){
-        //   this.loader = false;
+        if(res.data.success){
+          this.loader = false;
 
-        // }
-        console.log(res)
+        }
+        // console.log(res)
         this.projects = res.data.results.data;
         this.apiLink = res.data.results.links;
       })
     }
   },
-  changePage(numberPage){
-    //console.log(numberPage)
+  changePage(numberPage) {
+    console.log(numberPage)
 
     this.apiPage = numberPage;
 
     //una volta che ho lo stesso numero della pagina rifaccio la chimta api
     this.apiCall();
-    },
+  },
 
 
 
-  
+
 
 }
 </script>
 
 <template>
-<div class="container py-5 display-2 ">
-  Progetto di gruppo
-  <ul>
-    <li v-for="project in projects">
-      {{ project.name }}
-      
-    </li>
-  </ul>
+  <div class="container py-5 ">
+    Progetto di gruppo
+    <ul>
+      <li v-for="project in projects">
+        {{ project.name }}
 
-  
-</div>
+      </li>
+      <li v-for="project in projects">
+        {{ project.type.description }}
+
+      </li>
+    </ul>
+
+
+  </div>
+  <div class="container ">
+    <nav>
+      <ul class="d-flex gap-2 text-white">
+        
+        <li v-for="apiLink in apiLinks" v-html="link.label" @click="changePage(link.label)"
+          :class="link.label == apiPage ? 'active' : ''">
+
+        </li>
+      </ul>
+    </nav>
+  </div>
+
 </template>
 
-<style >
+<style lang="scss">
+nav {
+  ul {
 
-</style>
+    list-style-type: none;
+
+    li {
+
+      padding: 8px;
+
+      text-decoration: none;
+      color: white;
+
+      transition: all .3s ease;
+
+      cursor: pointer;
+
+      &:hover,
+      &.active {
+        background-color: rgba(255, 255, 255, 0.4);
+        color: white;
+      }
+
+    }
+  }
+}
+  </style>
