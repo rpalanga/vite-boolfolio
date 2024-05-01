1475f20642d1b298eab8312661f4c0eb39ce9251
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-01 23:11:28 +0200

    single project

---
+++ src/pages/SingleProject.vue
@@ -0,0 +1,50 @@
+
+<script>
+
+
+import axios from 'axios';
+
+export default {
+    name : 'SingleProject',
+
+    data() {
+        return {
+
+            post : null,
+
+            baseApiUrl: 'http://127.0.0.1:8000/api',
+        } 
+    },
+
+    mounted() {
+       console.log(this.$route)
+        axios.get(this.baseApiUrl + '/projects/11').then(res=> {
+
+            console.log(res.data.project);
+            this.post = res.data.project;
+        });
+    },
+
+    methods:{
+
+    },
+}
+
+</script>
+
+<template>
+
+    <div 
+    v-if="post"
+    class="container"> 
+       <h1> {{post.name}}</h1>
+       <p> {{post.description}}</p>
+
+
+    </div>
+    
+</template>
+
+<style lang="sass" scoped>
+
+</style>
