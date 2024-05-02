05e2ba85c3a9b482ea20a6754c2e627cdd48727d
roberto-palanga <r.palanga94@gmail.com>
2024-05-02 19:08:00 +0200

    Creazione single-page

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -9,8 +9,8 @@ export default {
     data() {
         return {
 
-            post : null,
-            postSlug: '',
+            project : null,
+            projectSlug: '',
 
             baseApiUrl: 'http://127.0.0.1:8000/api',
         } 
@@ -19,16 +19,16 @@ export default {
     mounted() {
        // console.log(this.$route.params.id);
        // this.postId = this.$route.params.id;
-       this.postSlug = this.$route.params.slug;
+       this.projectSlug = this.$route.params.slug;
 
-       console.log('chiamata api:', this.baseApiUrl + '/projects/' + this.postSlug)
-        axios.get(this.baseApiUrl + '/projects/' + this.postSlug).then(res=> {
+       console.log('chiamata api:', this.baseApiUrl + '/projects/' + this.projectSlug)
+        axios.get(this.baseApiUrl + '/projects/' + this.projectSlug).then(res=> {
 
            // console.log(res.data.project);
 
            if(res.data.success){
             // se c'è il post
-               this.post = res.data.project;
+               this.project = res.data.project;
            } else {
             // torno alla pagina iniziale
             this.$router.push({ name: 'home' })
@@ -46,10 +46,17 @@ export default {
 <template>
 
     <div 
-    v-if="post"
-    class="container"> 
-       <h1> {{post.name}}</h1>
-       <p> {{post.description}}</p>
+    v-if="project"
+    class="container d-flex justify-content-center"> 
+       
+       <div class="card col-12 my-4" style="width: 500px;height: 550px;">
+        <img :src=" 'http://localhost:8000/storage/'  + project.image " class=" card-img-top object-fit-cover" style="height: 300px; object-position: top; " alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{ project.name }} </h5>
+          <p class="card-text">{{ project.description }}</p>
+           
+        </div>
+      </div>
 
 
     </div>
