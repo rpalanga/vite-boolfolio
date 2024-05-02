07d6d8b76051b923e7e79f0fbd61fc3da35c5a71
Luca <lucafagiolini@live.it>
2024-05-02 17:09:23 +0200

    Footer base layout

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+    name: "Appfooter",
+};
+</script>
+
+
+<template>
+    <div class="text-center container">
+        <hr>
+        <div class="d-flex gap-5">
+
+
+            <div class="">
+                <h5 class="">Special title treatment</h5>
+                <p class="">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+
+            <div class="d-flex flex-column">
+                <h5>Download</h5>
+                <div class="d-flex flex-column align-items-start">
+                    <span>windows app</span>
+                    <span>mac app</span>
+                    <span>linux app</span>
+                    <span>desktop app</span>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column">
+                <h5>Products</h5>
+                <div class="d-flex flex-column align-items-start">
+                    <span>web</span>
+                    <span>app</span>
+                    <span>software</span>
+                    <span>ecommerce</span>
+                </div>
+            </div>
+
+            <div>
+                <h5>get in touch</h5>
+
+            </div>
+
+        </div>
+        <hr>
+        <div class="">
+            Portfolio created by
+            <span class="badge rounded-pill text-bg-success">
+                <a href="">Roberto</a>
+            </span>
+            <span class="badge rounded-pill text-bg-danger">
+                <a href="">Gian Marco</a>
+            </span>
+            <span class="badge rounded-pill text-bg-warning">
+                <a href="">Luca</a>
+            </span>
+        </div>
+        <hr>
+        <div class="card-footer text-body-secondary">
+            Boolean class 118
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped lang="scss"></style>
